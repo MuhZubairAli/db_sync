386f9c8155a5562e05eabfc015a22369b0bc4fdc
Muhammad Zubair <muhzubairali@gmail.com>
2020-03-17 15:37:01 +0500

    check.php

---
+++ check.php
@@ -0,0 +1,43 @@
+<?php
+$serverName = "pslm.pbos.gov.pk";
+$connectionOptions = array(
+    "database" => "PSLM1920",
+    "uid" => "pslm",
+    "pwd" => 'p$lm!920'
+);
+
+// Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if ($conn === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+
+// Select Query
+$tsql = "SELECT @@Version AS SQL_VERSION";
+
+// Executes the query
+$stmt = sqlsrv_query($conn, $tsql);
+
+// Error handling
+if ($stmt === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+
+while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+    echo $row['SQL_VERSION'] . PHP_EOL;
+}
+
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
+
+function formatErrors($errors)
+{
+    // Display errors
+    echo "Error information: <br/>";
+    foreach ($errors as $error) {
+        echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
+        echo "Code: ". $error['code'] . "<br/>";
+        echo "Message: ". $error['message'] . "<br/>";
+    }
+}
+?>

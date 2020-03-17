f3369005de3bad7aafc065b9304daee5d54ac52a
Muhammad Zubair <muhzubairali@gmail.com>
2020-03-17 20:59:50 +0500

    check_pdo.php added

---
+++ check_pdo.php
@@ -0,0 +1,47 @@
+<?php
+$serverName = "pslm.pbos.gov.pk";
+$connectionOptions = array(
+    "database" => "PSLM1920",
+    "uid" => "pslm",
+    "pwd" => 'p$lm!920'
+);
+
+// Establishes the connection
+//$conn = sqlsrv_connect($serverName, $connectionOptions);
+$conn = new PDO("sqlsrv:server=$serverName;Database={$connectionOptions['database']}", $connectionOptions['uid'], $connectionOptions['pwd']);
+if ($conn === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+
+// Select Query
+$tsql = "SELECT * FROM SECTION_A ORDER BY prcode OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY";
+
+// Executes the query
+//$stmt = sqlsrv_query($conn, $tsql);
+$stmt = $conn->query($tsql);
+
+// Error handling
+if ($stmt === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+
+//while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+while($row = $stmt->fetch()){    
+	var_dump($row);
+	echo PHP_EOL;
+}
+
+//sqlsrv_free_stmt($stmt);
+//sqlsrv_close($conn);
+
+function formatErrors($errors)
+{
+    // Display errors
+    echo "Error information: <br/>";
+    foreach ($errors as $error) {
+        echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
+        echo "Code: ". $error['code'] . "<br/>";
+        echo "Message: ". $error['message'] . "<br/>";
+    }
+}
+?>
